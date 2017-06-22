80c1b7011273c8db94691bd48aba906c4b118c15
rdad <akindofceasar@gmail.com>
2017-06-23 00:58:48 +0200

    added loop to performance.js : proceed 10 images now

--- node_version/chrono.js
+++ node_version/chrono.js
@@ -1,17 +1,23 @@
-
 let chrono_start = 0,
-    chrono_end = 0;
+  chrono_end = 0,
+  amount_total = 0;
 
 function start() {
-    chrono_start = Date.now();
+  chrono_start = Date.now();
 }
 
 function stop() {
-    chrono_end = Date.now();
+  chrono_end = Date.now();
 }
 
 function get_result() {
-    return chrono_end - chrono_start;
+  let t = chrono_end - chrono_start;
+  amount_total += t;
+  return t;
 }
 
-module.exports = { start, stop, get_result };
+function get_total() {
+  return amount_total;
+}
+
+module.exports = { start, stop, get_result, get_total };
--- node_version/performance.js
+++ node_version/performance.js
@@ -1,62 +1,123 @@
+const fs = require("fs");
+const getPixels = require("get-pixels");
+const savePixels = require("save-pixels");
 
-const fs = require('fs');
-const getPixels = require('get-pixels');
-const savePixels = require('save-pixels');
-
-const filter = require('./filters/filter');
-const chrono = require('./chrono');
-const pixel_container = require('./lib/pixel_container');
+const filter = require("./filters/filter");
+const chrono = require("./chrono");
+const pixel_container = require("./lib/pixel_container");
 
 const folder = {
-    source: "assets/performance_source/",
-    filtered: "assets/performance_filtered/"
+  source: "assets/performance_source/",
+  filtered: "assets/performance_filtered/"
 };
 
 let id = 0;
 
-console.log("C64image node version performance test","10 images to proceeds ...");
+console.log(
+  "C64image node version performance test",
+  "10 images to proceeds ..."
+);
 
-function proceed_image(){
+function proceed_image() {
+  getPixels(`${folder.source}portrait_${id}.jpg`, (err, data) => {
+    if (err) {
+      console.log("Bad image path");
+      return;
+    }
 
-    getPixels(`${folder.source}portrait_${id}.jpg`, (err, data) => {
+    pixel_container.init(data);
+    filter.init(pixel_container);
 
-        if (err) {
-            console.log("Bad image path")
-            return
+    chrono.start();
+
+    for (let i = 1; i < pixel_container.get_width() - 1; ++i) {
+      for (let j = 1; j < pixel_container.get_height() - 1; ++j) {
+        switch (id) {
+          case 0:
+            filter.palette.on_pixel(i, j, { name: "gameboy" });
+            filter.pixelate.on_pixel(i, j, { scale: 50 });
+            break;
+
+          case 1:
+            filter.pixelate.on_pixel(i, j, { scale: 30 });
+            filter.palette.on_pixel(i, j, { name: "c64" });
+            break;
+
+          case 2:
+            filter.grayscale.on_pixel(i, j, { name: "shade" });
+            filter.dither.on_pixel(i, j, { name: "jjn" });
+            break;
+
+          case 3:
+            filter.pixelate.on_pixel(i, j, { scale: 100 });
+            break;
+
+          case 4:
+            filter.contrast.on_pixel(i, j, { amount: 120 });
+            filter.dither.on_pixel(i, j, { name: "atkinson" });
+            filter.palette.on_pixel(i, j, { name: "black_white" });
+            break;
+
+          case 5:
+            filter.palette.on_pixel(i, j, { name: "vic20" });
+            filter.pixelate.on_pixel(i, j, { scale: 20 });
+            break;
+
+          case 6:
+            //filter.contrast.on_pixel(i, j, { amount: 140 });
+            filter.dither.on_pixel(i, j, { name: "bayer4" });
+            filter.grayscale.on_pixel(i, j, { name: "decomposition_max" });
+            break;
+
+          case 7:
+            filter.dither.on_pixel(i, j, { name: "floyd_steinberg" });
+            filter.palette.on_pixel(i, j, { name: "web" });
+            break;
+
+          case 8:
+            filter.palette.on_pixel(i, j, { name: "nes" });
+            filter.dither.on_pixel(i, j, { name: "bayer8" });
+            break;
+
+          case 9:
+            filter.palette.on_pixel(i, j, { name: "c64" });
+            filter.dither.on_pixel(i, j, { name: "floyd_steinberg" });
+            break;
         }
+        //filter.to_gray.on_pixel(i,j);
+        //filter.dither.on_pixel(i,j, {name: 'floyd_steinberg'});
+        //filter.palette.on_pixel(i,j, {name: 'cga'});
 
-        pixel_container.init(data);
-        filter.init(pixel_container);
+        //filter.to_gray.on_pixel(i,j);
 
-        chrono.start();
+        // filter.dither.on_pixel(i,j, {name: 'floyd_steinberg'});
+      }
+    }
 
-        for (let i = 1; i < pixel_container.get_width() - 1; ++i) {
-            for (let j = 1; j < pixel_container.get_height() - 1; ++j) {
+    chrono.stop();
 
-                //filter.to_gray.on_pixel(i,j);
-                //filter.dither.on_pixel(i,j, {name: 'floyd_steinberg'});
-                //filter.palette.on_pixel(i,j, {name: 'cga'});
+    let file_type = "png"; //file.split('.').pop();
 
-                filter.contrast.on_pixel(i,j, {amount: 100});
-                filter.dither.on_pixel(i,j, {name: 'bayer4'});
-                
+    var result_file = fs.createWriteStream(
+      `${folder.filtered}portrait_${id}.jpg`
+    );
+    savePixels(pixel_container.get_pixels(), file_type, { quality: 90 }).pipe(
+      result_file
+    );
 
-                //filter.to_gray.on_pixel(i,j);
+    console.log(
+      `${id} ${pixel_container.get_width() *
+        pixel_container.get_height()} pixels filtered in ${chrono.get_result()} ms`
+    );
 
-                // filter.dither.on_pixel(i,j, {name: 'floyd_steinberg'});
-                //filter.pixelate.on_pixel(i, j, { scale: 30 });
-            }
-        }
-
-        chrono.stop();
-
-        let file_type = 'png'; //file.split('.').pop();
-
-        var result_file = fs.createWriteStream(`${folder.filtered}portrait_${id}.jpg`);
-        savePixels(pixel_container.get_pixels(), file_type, { quality: 90 }).pipe(result_file);
-
-        console.log(`${id} ${pixel_container.get_width() * pixel_container.get_height()} pixels filtered in ${chrono.get_result()} ms`);
-    });
+    // loop;
+    id++;
+    if (id < 10) {
+      proceed_image();
+    } else {
+      console.log(`::: Total time : ${chrono.get_total()} ms`);
+    }
+  });
 }
 
-    proceed_image();
+proceed_image();
